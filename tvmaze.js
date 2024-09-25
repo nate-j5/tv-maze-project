5071fc6c5da389a6309fe3fb074658323901b199
nate <natejohnson0592@gmail.com>
2024-09-25 17:16:10 -0500

    https

--- tvmaze.js
+++ tvmaze.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const MISSING_IMAGE_URL = "https://tinyurl.com/missing-tv";
-const TVMAZE_API_URL = "http://api.tvmaze.com/";
+const TVMAZE_API_URL = "https://api.tvmaze.com/";
 
 const $showsList = $("#showsList");
 const $episodesList = $("#episodesList");
